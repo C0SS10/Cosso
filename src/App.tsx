a8a0f9629b6bb0062acc4731309fcf1598fe9ac1
Esteban <estebancg12@hotmail.com>
2026-03-04 18:56:03 -0500

    Add Experience section with dynamic content and new icons 🎨

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import ThemeToggle from "@components/ui/ThemeToggle"
 import About from "@components/sections/About"
+import Experience from "@components/sections/Experience"
 
 function App() {
 
   return (
-    <main className="grid grid-cols-3 grid-rows-3 gap-4 justify-items-center min-h-screen px-32 py-8 transition-colors duration-300 ease-linear">
+    <main className="grid grid-cols-3 grid-rows-3 gap-4 justify-items-center px-32 py-8 transition-colors duration-300 ease-linear h-screen">
       <ThemeToggle />
       <About />
+      <Experience />
     </main>
   )
 }
