fbd54f605bfbb55f11ed99341ba0a43e664e0048
Esteban <estebancg12@hotmail.com>
2026-06-22 07:42:23 -0500

    Merging some changes that I did in tests branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import '@styles/global.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Cosso" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
